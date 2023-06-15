64e46b3db27b797f400cc0ee11b7268a8b373085
Jakub Levanszky <j.levanszky@gmail.com>
2023-06-15 22:28:00 +0200

    basic custom input

---
+++ src/components/InputCustom.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="input-wrapper">
+    <label v-if="label"> {{ label }}</label>
+    <input
+      type="text"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+      v-bind="$attrs"
+    />
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  label: {
+    type: [Boolean, String]
+  },
+  modelValue: {
+    type: String,
+    default: ''
+  }
+})
+</script>
+
+<style lang="scss" scoped></style>
