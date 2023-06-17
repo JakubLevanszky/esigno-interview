1817852eed390e4a721d5e5f555cefd81520db5b
Jakub Levanszky <j.levanszky@gmail.com>
2023-06-17 09:03:22 +0200

    axios + just structure for POST

--- src/components/RegistrationFlow.vue
+++ src/components/RegistrationFlow.vue
@@ -1,6 +1,32 @@
 <script setup>
 import InputCustom from './InputCustom.vue'
 import { ref } from 'vue'
+import axios from 'axios'
+
+const methods = {
+  handleSubmit() {
+    console.log('handlesubmit')
+    axios
+      .post(
+        'https://cors-anywhere.herokuapp.com/https://userhw.sandbox.esigno.io/registration',
+        {
+          email: email
+        },
+        {
+          headers: {
+            Accept: '*/*',
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+          }
+        }
+      )
+      .then((respone) => {
+        const data = respone.data
+        console.log(respone)
+        console.log(data)
+      })
+  }
+}
 
 const email = ref('')
 </script>
@@ -13,7 +39,7 @@ const email = ref('')
       >Na e-mail Vám bude zaslán ověřovací kód. Po ověření prosím pokračujte v dalších krocích
       registrace.</span
     >
-    <form>
+    <form @submit.prevent="methods.handleSubmit">
       <InputCustom v-model="email" type="email" label="E-mail" required />
       <button type="submit">Pokračovat</button>
     </form>
